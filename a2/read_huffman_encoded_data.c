efe27f3fa5f4261db2ef62a246f22e6cbd934acf
Isaac Bercovitz Green <isaac@gritz.ca>
2024-10-22 21:07:10 -0400

    Added file reading function

---
+++ a2/read_huffman_encoded_data.c
@@ -0,0 +1,33 @@
+#include "read_huffman_encoded_data.h"
+
+unsigned char *read_huffman_encoded_data(
+    char *compressed_file_name_ptr,
+    int *image_width,
+    int *image_height,
+    int *max_gray_value,
+    int *number_of_nodes,
+    struct node **huffman_node,
+    long int *length_of_encoded_image_array
+)
+{
+    FILE *file = fopen(compressed_file_name_ptr, "r");
+    if (file == NULL) {
+        printf("Could not open compressed file: %s\n", compressed_file_name_ptr);
+        return NULL;
+    }
+
+    fscanf(file, "%d\n", image_width);
+    fscanf(file, "%d\n", image_height);
+    fscanf(file, "%d\n", max_gray_value);
+
+    fscanf(file, "%d\n", number_of_nodes);
+    fread(huffman_node, sizeof(struct node), *number_of_nodes, file);
+
+    fscanf(file, "%li\n", length_of_encoded_image_array);
+
+    unsigned char *encoded_data = (unsigned char *) calloc (*length_of_encoded_image_array, sizeof(unsigned char));
+    fread(encoded_data, sizeof(unsigned char), *length_of_encoded_image_array, file);
+
+    fclose(file);
+    return encoded_data;
+}
---
+++ a2/read_huffman_encoded_data.h
@@ -0,0 +1,9 @@
+#ifndef READ_HUFFMAN_ENCODED_DATA_H
+#define READ_HUFFMAN_ENCODED_DATA_H
+
+#include <stdio.h>
+#include "generate_huffman_nodes.h"
+
+unsigned char *read_huffman_encoded_data(char*, int*, int*, int*, int*, struct node**, long int*);
+
+#endif
